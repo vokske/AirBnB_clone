a27442de839486d077ff2925eb0fca3644f3ea10
vokske <kevinmuigai2@gmail.com>
2024-08-18 16:01:27 +0300

    Class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,33 +2,23 @@
 
 import uuid
 from datetime import datetime
-from models.base_model import BaseModel
-from models import storage
 
-class BaseModel(object):
+class BaseModel:
 
     """ Class BaseModel
     Defines all common attributes/methods for other classes
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initialize a Base instance.
 
         Args:
             - *args: List of arguments to the function
             - **kwargs: Keyworded list of arguments (dict) to the function
         """
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.fromisoformat(value))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
 
     def __str__(self):
@@ -41,7 +31,6 @@ class BaseModel(object):
         with the current datetime.
         """
         self.updated_at = datetime.now()
-        storage.save()
 
 
     def to_dict(self):
