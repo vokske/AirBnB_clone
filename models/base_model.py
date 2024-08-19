9253fa4941702011c8b343b33f84d4733f112faf
vokske <kevinmuigai2@gmail.com>
2024-08-19 18:04:40 +0300

    Updated BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,18 @@ class BaseModel:
         - to_dict
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor for class BaseModel."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
 
     def __str__(self):
