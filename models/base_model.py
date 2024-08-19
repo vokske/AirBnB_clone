9b83874e85843f45dc7660a2051627f3d98956e2
vokske <kevinmuigai2@gmail.com>
2024-08-19 21:29:45 +0300

    Update base_model module

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 """Module contains class BaseModel."""
 import uuid
 from datetime import datetime
+from models import storage
+
 
 class BaseModel:
     """Defines all common attributes/methods for other classes.
@@ -28,18 +30,17 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
+            storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation of an instance."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-
     def save(self):
         """updates the public instance attribute
         updated_at with the current datetime."""
         self.updated_at = datetime.now()
-
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values
