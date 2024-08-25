a5774e886c3df9494ad13fd3d9da7b5b26e9915e
vokske <kevinmuigai2@gmail.com>
2024-08-26 02:13:00 +0300

    Updated command interpreter

--- console.py
+++ console.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Contains the class for a simple interpreter."""
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     """Simple command interpreter"""
@@ -19,10 +22,130 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """Handle execution of empty line."""
+        """Handles execution of empty line."""
         pass
 
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it
+        to a JSON file and prints the id.
+        """
+        if not line:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            a = BaseModel()
+            a.save()
+            print(a.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        based on the class name and id.
+        """
+        if not line:
+            print("** class name is missing **")
+            return
+        args = line.split(' ')
+
+        if len(args) == 1:
+            print("** instance id is missing **")
+            return
+        else:
+            class_name = args[0]
+            instance_id = args[1]
+            if class_name not in storage.classes():
+                print("** class doesn't exist **")
+                return
+            else:
+                key = f"{class_name}.{instance_id}"
+                instance = storage.all().get(key)
+                if instance is None:
+                    print("** no instance found **")
+                    return
+                print(instance)
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name
+        and id and saves the change into the JSON file.
+        """
+        if not line:
+            print("** class name missing **")
+            return
+        else:
+            args = line.split(' ')
+            class_name = args[0]
+            instance_id = args[1]
+            if class_name not in storage.classes():
+                print("** class name doesn't exist **")
+                return
+            elif not instance_id:
+                print("** instance id missing **")
+                return
+            else:
+                key = f"{class_name}.{instance_id}"
+                instance = storage.all().get(key)
+                if not instance:
+                    print("** no instance found **")
+                    return
+                del storage.all()[key]
+                storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of all
+        instances based or not on the class name.
+        """
+        if not line or line in storage.classes():
+            instances = [str(value) for key, value in storage.all().items()]
+            print(instances)
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, line):
+        """Updates an instance based on the class name and id by adding
+        or updating an attribute and saves the change into the JSON file.
+        """
+        if not line:
+            print("** class name missing **")
+
+        else:
+            args = line.split(" ")
+            class_name = args[0]
+
+            if class_name not in storage.classes():
+                print("** class name doesn't exist **")
+
+            elif len(args) < 2:
+                print("** instance id missing **")
+
+            elif len(args) < 3:
+                print("** attribute name missing **")
+
+            elif len(args) < 4:
+                print("** value missing **")
+
+            else:
+                instance_id = args[1]
+                attr_name = args[2]
+                attr_value = args[3]
+
+                if type(attr_name) == int:
+                    attr_value = int(args[3].strip('"'))
+                elif type(attr_name) == float:
+                    attr_value = float(args[3].strip('"'))
+                elif type(attr_name) == str:
+                    attr_value = str(args[3].strip('"'))
+
+                key = f"{class_name}.{instance_id}"
+                instance = storage.all().get(key)
+
+                if not instance:
+                    print("** no instance found **")
+
+                else:
+                    setattr(instance, attr_name, attr_value)
+                storage.save()
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+storage._FileStorage__objects.clear()
